01c497dde4bb32bdcd5aba98b6d281ba1e22dadd
reiracm <reiracm@gmail.com>
2020-05-20 08:30:14 -0600

    Update p1_metodo_nuevo.py

--- p1_metodo_nuevo.py
+++ p1_metodo_nuevo.py
@@ -10,13 +10,15 @@ def ssm(func,xo,tol):
                                   'exp': exp, 'sqrt': sqrt,
                                   'arccos': acos, 'sin': sin, 'tan': tan, 
                                   'ln': np.log, 'log10': math.log10})
-        
+
+     #Las iteraciones inician en cero y los arreglos de iteraciones en vacio   
     xC = xo
     itera = 0
     error = [ ]
     iteracion = [ ]
     tempTol = 1
-        
+
+    #Condicion de parada
     while(tempTol >= tol):
 
         itera += 1
@@ -26,20 +28,23 @@ def ssm(func,xo,tol):
             
         y = xC - ((fx**2)/(z-fx))
         fy = f(y)
-          
+
+        #Calculo de la nueva aproximacion
         xAprox = xC - (fx**3)/((z-fx)*(fx-fy))
         tempTol = abs(xAprox-xC)
         xC = xAprox
 
+        #Concatenacion de iteraciones
         error.append(tempTol)
         iteracion.append(itera)
-            
-    #if(graf == 1):
+
+    #Grafica
     plt.plot(iteracion,error)
     plt.ylabel('Errores')
     plt.xlabel('Iteraciones')
     plt.show()
 
     return xAprox, itera
+
 
 ssm("((log10(7/x)/((1/10)*ln(10)))+(x*(6-x)/(((2*10**2*arccos(x/20)-x*sqrt(10**2-(x**2/4)))**2/(2*(40/ln(10))**2))*((1/(20**2*arccos(x/20)-x*sqrt(10**2-(x**2/4))))+(1/pi*10**2)))))",1, 10**(-10))
